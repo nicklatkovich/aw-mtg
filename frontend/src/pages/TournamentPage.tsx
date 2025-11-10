231dc72db3d748323768659480c7dd7341347b04
NickLatkovich <nicklatkovich@gmail.com>
2025-11-10 23:16:37 +0300

    Improve tournament page for rounds > 5

--- frontend/src/pages/TournamentPage.tsx
+++ frontend/src/pages/TournamentPage.tsx
@@ -35,56 +35,66 @@ const TournamentPage: React.FC = () => {
                   {melee}
                 </a>
               ) : null}
-              <table style={{ marginTop: 32 }}>
-                <thead>
-                  <tr>
-                    <th>Rank</th>
-                    <th>Player</th>
-                    <th>Points</th>
-                    {displayDecks ? <th>Deck</th> : null}
-                    {displayMatchRecord ? <th>Match Record</th> : null}
-                    {displayGameRecord ? <th>Game Record</th> : null}
-                    {...Array.from({ length: rounds })
-                      .fill(0)
-                      .map((_, i) => <th key={i.toString(10)}>Round #{i + 1}</th>)}
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.standings.map((s) => (
-                    <tr key={s.rank}>
-                      <th>{s.rank}</th>
-                      <th>
-                        <Link to={`/player/${s.player.id}`}>{s.player.display_name}</Link>
-                      </th>
-                      <th>{s.points}</th>
-                      {displayDecks ? (
-                        <th className="left">
-                          <DeckComponent deck={s.deck} />
-                        </th>
-                      ) : null}
-                      {displayMatchRecord ? <th>{s.match_record}</th> : null}
-                      {displayGameRecord ? <th>{s.game_record}</th> : null}
-                      {...Array.from({ length: rounds })
-                        .fill(0)
-                        .map((_, i) => {
-                          const r = s.rounds?.[i];
-                          if (!r) return <th>-</th>;
-                          if (r === 'bye') return <th className="round--bye">bye</th>;
-                          let className = '';
-                          if (r.res === -1) className = 'round--lose';
-                          else if (r.res === 0) className = 'round--draw';
-                          else if (r.res === 1) className = 'round--win';
-                          const record = r.record ?? (r.res === -1 ? 'lose' : r.res === 0 ? 'draw' : 'win');
-                          return (
-                            <th className={className}>
-                              {r.pod === undefined ? r.vs : String.fromCharCode(64 + r.pod)}: {record}
-                            </th>
-                          );
-                        })}
-                    </tr>
+              <div
+                style={{
+                  marginTop: 32,
+                  gridTemplateColumns: [
+                    'auto',
+                    'auto',
+                    'auto',
+                    ...(displayDecks ? ['auto'] : []),
+                    ...(displayMatchRecord ? ['auto'] : []),
+                    ...(displayGameRecord ? ['auto'] : []),
+                    ...Array.from({ length: rounds }, () => 'minmax(0, 1fr)'),
+                  ].join(' '),
+                }}
+                className="grid-table"
+              >
+                <div style={{ display: 'contents' }}>
+                  <div className="cell">Rank</div>
+                  <div className="cell">Player</div>
+                  <div className="cell">Points</div>
+                  {displayDecks ? <div className="cell">Deck</div> : null}
+                  {displayMatchRecord ? <div className="cell">Match Record</div> : null}
+                  {displayGameRecord ? <div className="cell">Game Record</div> : null}
+                  {Array.from({ length: rounds }).map((_, i) => (
+                    <div key={i.toString(10)} className={`cell ${rounds > 5 ? 'small-font' : ''}`}>
+                      Round #{i + 1}
+                    </div>
                   ))}
-                </tbody>
-              </table>
+                </div>
+                {data.standings.map((s) => (
+                  <div key={s.rank} style={{ display: 'contents' }}>
+                    <div className="cell">{s.rank}</div>
+                    <div className="cell left">
+                      <Link to={`/player/${s.player.id}`}>{s.player.display_name}</Link>
+                    </div>
+                    <div className="cell">{s.points}</div>
+                    {displayDecks ? (
+                      <div className="cell left">
+                        <DeckComponent deck={s.deck} />
+                      </div>
+                    ) : null}
+                    {displayMatchRecord ? <div className="cell">{s.match_record}</div> : null}
+                    {displayGameRecord ? <div className="cell">{s.game_record}</div> : null}
+                    {Array.from({ length: rounds }).map((_, i) => {
+                      let className = `cell ${rounds > 5 ? 'small-font' : ''}`;
+                      const r = s.rounds?.[i];
+                      if (!r) return <div className={className}>-</div>;
+                      if (r === 'bye') return <div className={`${className} round--bye`}>bye</div>;
+                      if (r.res === -1) className += ' round--lose';
+                      else if (r.res === 0) className += ' round--draw';
+                      else if (r.res === 1) className += ' round--win';
+                      const record = r.record ?? (r.res === -1 ? 'lose' : r.res === 0 ? 'draw' : 'win');
+                      return (
+                        <div key={i.toString(10)} className={className}>
+                          {r.pod === undefined ? r.vs : String.fromCharCode(64 + r.pod)}: {record}
+                        </div>
+                      );
+                    })}
+                  </div>
+                ))}
+              </div>
             </>
           );
         }}
